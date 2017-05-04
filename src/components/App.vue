6d8da0e24e0ede4fa9a318096c2f695af6aff330
Candyaou <paspana.assarasee@gmail.com>
2017-05-05 00:42:54 +0700

    Create Movie.vue to show Poster and name

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,24 @@
 <template>
     <div class="box">
-        <h3 class="title">App</h3>
+        <h3 class="title">App: {{ movieId }}</h3>
+        <Movie @chooseMovie="handleChooseMovie" :movieId="movieId"/>
     </div>
 </template>
+
+<script>
+import Movie from 'Components/Movie.vue'
+
+export default{
+    components: { Movie },
+    data(){
+        return{
+            movieId: ''
+        }
+    },
+    methods: {
+        handleChooseMovie(movieId){
+            this.movieId = movieId
+        }
+    }
+}
+</script>
