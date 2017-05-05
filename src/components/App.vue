b1c02367b1d33858b21f938f14b15ee5af201192
Candyaou <paspana.assarasee@gmail.com>
2017-05-05 15:18:46 +0700

    Sync choosen seat with firebase

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,13 +11,25 @@
             :movieId="movieId" 
             @chooseSeat="handleChooseSeat"
             :selectSeats="selectSeats"
+            :firebaseSeat = "firebaseSeat"
         />
     </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+import _ from 'lodash'
 import Movie from 'Components/Movie.vue'
 import Seat from 'Components/Seat.vue'
+import { pushToArray } from 'Others/lib'
+
+const config = {
+    databaseURL: "https://vuemovie-81ecf.firebaseio.com",
+}
+
+firebase.initializeApp(config)
+
+const db = firebase.database()
 
 export default{
     components: { Movie, Seat },
@@ -25,6 +37,7 @@ export default{
         return{
             movieId: '',
             selectSeats: [],
+            firebaseSeat: [],
             status: {count: 0, price: 0}
         }
     },
@@ -39,16 +52,34 @@ export default{
                 }
             }
             this.movieId = movieId
+
+            const movieRef = db.ref('/').child(this.movieId)
+            movieRef.on('value', snapshot =>{
+                // console.log(snapshot.val())
+                const seats = snapshot.val()
+                
+                this.firebaseSeat = []
+
+                _.forOwn(seats, s => {
+                    // console.log(s)
+                    pushToArray(s, this.firebaseSeat)
+                })
+                // console.log(this.firebaseSeat.length)
+            })
         },
         handleChooseSeat(seat){
-            const ids = this.selectSeats.map(s => s.id)
-            const idx = ids.indexOf(seat.id)
+            // const ids = this.selectSeats.map(s => s.id)
+            // const idx = ids.indexOf(seat.id)
 
-            if(idx === -1){
-                this.selectSeats.push(seat)
-            }else{
-                this.selectSeats.splice(idx,1)
-            }
+            // if(idx === -1){
+            //     this.selectSeats.push(seat)
+            // }else{
+            //     this.selectSeats.splice(idx,1)
+            // }
+            pushToArray(seat, this.selectSeats)
+
+            const movieRef = db.ref().child(this.movieId)
+            movieRef.push(seat)
 
             this.status = this.selectSeats.reduce((summary, s) => {
                 summary.count++
--- src/components/Seat.vue
+++ src/components/Seat.vue
@@ -3,7 +3,7 @@
         <template v-for="s in seats">
             <button 
                 :class="className(s)"
-                :disabled="s.seated"
+                :disabled="disabledText(s)"
                 @click="chooseSeat(s)"
                 >{{s.id}} {{s.price}}</button>
             <span>&nbsp;</span>
@@ -16,15 +16,25 @@ import movie from 'Others/movie.json'
 console.log(movie)
 
 export default {
-    props: [ 'movieId', 'selectSeats' ],
+    props: [ 'movieId', 'selectSeats', 'firebaseSeat' ],
     methods: {
+        disabledText(seat){
+            return seat.seated
+        },
         className(seat){
             const ids = this.selectSeats.map(s => s.id)
             const idx = ids.indexOf(seat.id)
 
+            const firebaseIds = this.firebaseSeat.map(s => s.id)
+            const firebaseIdx = firebaseIds.indexOf(seat.id)
+
             return[
                 'button',
-                { 'is-danger' : seat.seated, 'is-primary': idx != -1 }
+                { 
+                    'is-danger' : seat.seated, 
+                    'is-primary': idx != -1,
+                    'is-warning': firebaseIdx != -1 && idx === -1
+                }
             ]
         },
         chooseSeat(seat){
